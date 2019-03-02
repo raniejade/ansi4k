e16a13d426ca30afd60c4133c670c87805815ffe
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2019-03-02 21:37:55 +1100

    .

--- ci/jabba.ps1
+++ ci/jabba.ps1
@@ -1,8 +1,4 @@
-Set-ExecutionPolicy Unrestricted
-
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-Expression (
   Invoke-WebRequest https://github.com/shyiko/jabba/raw/master/install.ps1 -UseBasicParsing
 ).Content
-
-Set-ExecutionPolicy AllSigned
